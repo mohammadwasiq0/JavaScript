7f68354a25b1532ff06715ab57288b1aee91f884
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2024-10-08 17:01:22 +0000

    JS Comparison

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,23 @@
+// JS Comparison
+
+console.log(2 > 1); // true
+console.log(2 >= 1); // true
+console.log(2 < 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+console.log("1" == 1); // true
+console.log("2" == 1); // false
+console.log("02" == 1); // false
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+
+// ===
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+console.log(2 === 2); // true
+
