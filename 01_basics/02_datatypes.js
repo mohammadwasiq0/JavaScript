a5bc724513c4a5b4e9acf63c5654961123ddff78
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2024-10-08 16:13:31 +0000

    Data Type

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // We are using node.js, not browser
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+
+console.log("Mohammad Wasiq");
+
+console.log(3 + 3);
+
+let name = "Wasiq"
+let age = 24
+let isLoggedIn= false
+let state;
+
+console.log(typeof name);
+console.log(typeof age); 
+console.log(typeof isLoggedIn); 
+console.log(typeof state); 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+ 
